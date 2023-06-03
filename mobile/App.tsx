723e79608510d27ff7a834b53327d16deec4decd
RaffDv <rafael.vargas.emp@gmail.com>
2023-06-03 11:11:09 -0300

    [automatic] automatic commit

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { ImageBackground, Text } from 'react-native'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import BlurBg from './scr/public/assets/luz.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    BaiJamjuree_700Bold,
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-bold text-5xl text-zinc-50">Hello World!</Text>
+    <ImageBackground
+      source={BlurBg}
+      imageStyle={{
+        position: 'absolute',
+        left: '-100%',
+      }}
+      className="relative flex-1 items-center bg-gray-900"
+    >
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   )
 }
